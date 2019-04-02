d99bc3f6f10dd97d10ccf27c668abd6e365965f3
Ruochen Xu <ruochenx@philly.lti.cs.cmu.edu>
2019-04-02 15:07:41 -0400

    install tools

--- get-data-xnli.sh
+++ get-data-xnli.sh
@@ -9,11 +9,12 @@ set -e
 
 # data paths
 MAIN_PATH=$PWD
+EXP_PATH=exp/XNLI-15
 OUTPATH=$PWD/data/xnli
 XNLI_PATH=$PWD/data/xnli/XNLI-1.0
 PROCESSED_PATH=$PWD/data/processed/XLM15
-CODES_PATH=$MAIN_PATH/codes_xnli_15
-VOCAB_PATH=$MAIN_PATH/vocab_xnli_15
+CODES_PATH=$EXP_PATH/codes_xnli_15
+VOCAB_PATH=$EXP_PATH/vocab_xnli_15
 
 # tools paths
 TOOLS_PATH=$PWD/tools
@@ -42,41 +43,44 @@ if [ ! -d $OUTPATH/XNLI-1.0 ]; then
 fi
 
 # English train set
-echo "*** Preparing English train set ****"
-cat $OUTPATH/XNLI-MT-1.0/multinli/multinli.train.en.tsv | sed 's/\tcontradictory/\tcontradiction/g' > $XNLI_PATH/en.train
+# echo "*** Preparing English train set ****"
+# cat $OUTPATH/XNLI-MT-1.0/multinli/multinli.train.en.tsv | sed 's/\tcontradictory/\tcontradiction/g' > $XNLI_PATH/en.train
 
 # validation and test sets
-for lg in ar bg de el en es fr hi ru sw th tr ur vi zh; do
-
-  echo "*** Preparing $lg validation and test sets ***"
-  echo -e "premise\thypo\tlabel" > $XNLI_PATH/$lg.valid
-  echo -e "premise\thypo\tlabel" > $XNLI_PATH/$lg.test
-
-  # label
-  awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.dev.tsv  | cut -f2 > $XNLI_PATH/dev.f2
-  awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.test.tsv | cut -f2 > $XNLI_PATH/test.f2
-
-  # premise/hypothesis
-  awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.dev.tsv  | cut -f7 | $TOKENIZE $lg | python $LOWER_REMOVE_ACCENT > $XNLI_PATH/dev.f7
-  awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.dev.tsv  | cut -f8 | $TOKENIZE $lg | python $LOWER_REMOVE_ACCENT > $XNLI_PATH/dev.f8
-  awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.test.tsv | cut -f7 | $TOKENIZE $lg | python $LOWER_REMOVE_ACCENT > $XNLI_PATH/test.f7
-  awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.test.tsv | cut -f8 | $TOKENIZE $lg | python $LOWER_REMOVE_ACCENT > $XNLI_PATH/test.f8
-
-  paste $XNLI_PATH/dev.f7  $XNLI_PATH/dev.f8  $XNLI_PATH/dev.f2  >> $XNLI_PATH/$lg.valid
-  paste $XNLI_PATH/test.f7 $XNLI_PATH/test.f8 $XNLI_PATH/test.f2 >> $XNLI_PATH/$lg.test
-
-  rm $XNLI_PATH/*.f2 $XNLI_PATH/*.f7 $XNLI_PATH/*.f8
-done
+# echo "*** Preparing validation and test sets ***"
+# for lg in ar bg de el en es fr hi ru sw th tr ur vi zh; do
+# for lg in zh; do
+#
+#   echo "*** Preparing $lg validation and test sets ***"
+#   echo -e "premise\thypo\tlabel" > $XNLI_PATH/$lg.valid
+#   echo -e "premise\thypo\tlabel" > $XNLI_PATH/$lg.test
+#
+#   # label
+#   awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.dev.tsv  | cut -f2 > $XNLI_PATH/dev.f2
+#   awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.test.tsv | cut -f2 > $XNLI_PATH/test.f2
+#
+#   # premise/hypothesis
+#   awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.dev.tsv  | cut -f7 | $TOKENIZE $lg | python $LOWER_REMOVE_ACCENT > $XNLI_PATH/dev.f7
+#   awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.dev.tsv  | cut -f8 | $TOKENIZE $lg | python $LOWER_REMOVE_ACCENT > $XNLI_PATH/dev.f8
+#   awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.test.tsv | cut -f7 | $TOKENIZE $lg | python $LOWER_REMOVE_ACCENT > $XNLI_PATH/test.f7
+#   awk -v lg=$lg '$1==lg' $XNLI_PATH/xnli.test.tsv | cut -f8 | $TOKENIZE $lg | python $LOWER_REMOVE_ACCENT > $XNLI_PATH/test.f8
+#
+#   paste $XNLI_PATH/dev.f7  $XNLI_PATH/dev.f8  $XNLI_PATH/dev.f2  >> $XNLI_PATH/$lg.valid
+#   paste $XNLI_PATH/test.f7 $XNLI_PATH/test.f8 $XNLI_PATH/test.f2 >> $XNLI_PATH/$lg.test
+#
+#   rm $XNLI_PATH/*.f2 $XNLI_PATH/*.f7 $XNLI_PATH/*.f8
+# done
 
 rm -rf $PROCESSED_PATH/eval/XNLI
 mkdir -p $PROCESSED_PATH/eval/XNLI
 
-# Get BPE codes and vocab
-wget -c https://dl.fbaipublicfiles.com/XLM/codes_xnli_15 -P $MAIN_PATH
-wget -c https://dl.fbaipublicfiles.com/XLM/vocab_xnli_15 -P $MAIN_PATH
+# # Get BPE codes and vocab
+# wget -c https://dl.fbaipublicfiles.com/XLM/codes_xnli_15 -P $MAIN_PATH
+# wget -c https://dl.fbaipublicfiles.com/XLM/vocab_xnli_15 -P $MAIN_PATH
 
 # apply BPE codes and binarize the XNLI corpora
 for lg in ar bg de el en es fr hi ru sw th tr ur vi zh; do
+# for lg in ar; do
   for splt in train valid test; do
     if [ "$splt" = "train" ] && [ "$lg" != "en" ]; then
       continue
